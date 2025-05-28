7e82c28797284ca8bbd8b441a10ba3f89fdf3e94
chiranjib-swain <chiranjib-swain@github.com>
2025-05-28 18:22:16 +0530

    testing issue 870

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 // A sample component
 function Button({ label }) {
-    return `<button>${label}</button> `;//testing this button and
+    return `<button>${label}</button> `;//testing this button and issue 870
   }
   module.exports = Button;
